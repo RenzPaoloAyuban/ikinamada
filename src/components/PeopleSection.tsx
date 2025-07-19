d7615c9efffe5930a7c13f4149a752476bfea728
RenzPaoloAyuban <renzpaoloayuban01@gmail.com>
2025-07-19 21:45:41 +0800

    Edited the header for the People Section

--- src/components/PeopleSection.tsx
+++ src/components/PeopleSection.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import AnimatedTestimonialsDemo from './ui/animated-testimonials-demo'
+import { div } from 'motion/react-client'
 
 const PeopleSection = () => {
 
   return (
+   
     <div className="w-full h-200 flex flex-col items-center justify-center overflow-hidden gap-20">
+        <div className='text-black font-bold text-5xl'>
+          People behind Ikinamada
+        </div>
         <AnimatedTestimonialsDemo />
     </div>
   )
