61ca94eec906da681edfdf2743ae078b86020c09
RenzPaoloAyuban <renzpaoloayuban01@gmail.com>
2025-07-22 21:57:02 +0800

    modified footer section

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const FooterSection = () => {
   return (
-    <div>FooterSection</div>
+    <div className='relative flex flex-row bg-black w-full h-20 p-10 text-white justify-between items-center '>
+      <div>ikinamada logo</div>
+      <div>copyright</div>
+      <div>social media</div>
+    </div>
   )
 }
 
